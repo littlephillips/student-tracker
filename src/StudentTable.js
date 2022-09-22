bd8ad675bd43757d50fed39852011fed43ae0232
littlephillips <phillipkinuthia72@gmail.com>
2022-09-22 10:10:35 +0300

    updated the StudentTable component

--- src/StudentTable.js
+++ src/StudentTable.js
@@ -1,38 +1,25 @@
+import studentData  from './utility/studentData';
 
-import { Table} from 'antd';
-import studentData from './utility/studentData';
 
-const columns = [
-  {
-    title: 'id',
-    dataIndex: 'id',
-    key: 'id',
-  },
-  {
-    title: 'firstName',
-    dataIndex: 'firstName',
-    key: 'firstName',
-  },
-  {
-    title: 'lastName',
-    dataIndex: 'lastName',
-    key: 'lastName',
-  },
-  {
-    title: 'email',
-    dataIndex: 'email',
-    key: 'email',
-  },
-  {
-    title: 'gender',
-    dataIndex: 'gender',
-    key: 'gender',
-  },
-];
 
 export default function StudentTable(){
-  return(
-    < Table dataSource={studentData()} columns={columns} />
+  
+  return (
+    
+    <div class="table-container">
+      <table>
+        <caption>Our Client Data</caption>
+        <tbody>
+          {studentData().map(({id,firstName,lastName,email,gender}) => (
+            <tr key={id}>
+              <td>{firstName}</td>
+              <td>{lastName}</td>
+              <td>{email}</td>
+              <td>{gender}</td>
+            </tr>
+          ))}   
+    </tbody>
+    </table>
+    </div>
   )
 }
-
